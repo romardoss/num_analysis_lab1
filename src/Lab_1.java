ab2a59a334474f830a1fe4d42775993fce92e893
romardoss <romassuper@gmail.com>
2022-10-14 13:54:37 +0300

    input data added

--- src/Lab_1.java
+++ src/Lab_1.java
@@ -1,5 +1,15 @@
 public class Lab_1 {
+    int variant = 6;
+    double a = 0.2 * (variant - 5);
+    double b = a;
+    double[][] matrix = {
+            {8.30, 2.62+a, 4.10, 1.90, -10.65+b},
+            {3.92, 8.45, 8.78-a, 2.46, 12.21},
+            {3.77, 7.21+a, 8.04, 2.28, 15.45-b},
+            {2.21, 3.65-a, 1.69, 6.99, -8.35}
+    };
+
     public static void main(String[] args) {
-        
+
     }
 }
