da5c995b0f9297af9280297e26697df7de6848a7
romardoss <romassuper@gmail.com>
2022-10-14 14:18:02 +0300

    printMatrix method created

--- src/Lab_1.java
+++ src/Lab_1.java
@@ -1,8 +1,8 @@
 public class Lab_1 {
-    int variant = 6;
-    double a = 0.2 * (variant - 5);
-    double b = a;
-    double[][] matrix = {
+    static int variant = 6;
+    static double a = 0.2 * (variant - 5);
+    static double b = a;
+    static double[][] matrix = {
             {8.30, 2.62+a, 4.10, 1.90, -10.65+b},
             {3.92, 8.45, 8.78-a, 2.46, 12.21},
             {3.77, 7.21+a, 8.04, 2.28, 15.45-b},
@@ -10,6 +10,17 @@ public class Lab_1 {
     };
 
     public static void main(String[] args) {
+        printMatrix(matrix);
+    }
 
+    public static void printMatrix(double[][] m){
+        int n = m.length;
+        for (double[] doubles : m) {
+            System.out.print("| ");
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%.3f ", doubles[j]);
+            }
+            System.out.printf("| %.3f |\n", doubles[n]);     //last element
+        }
     }
 }
